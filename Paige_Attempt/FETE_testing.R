f9feb9c206f7bfdec92a03ad87e01db029ebc86d
paigepaulsen <35267511+paigepaulsen@users.noreply.github.com>
2024-05-13 07:23:41 -0400

    file uploads
    SOLAR work

--- Paige_Attempt/FETE_testing.R
+++ Paige_Attempt/FETE_testing.R
@@ -7,7 +7,7 @@ library(sf)
 # my map, first try 3 written coordinates
 # code starts direct from the example included in leastcostpath
 
-b <- terra::rast("C:\\Users\\Paige\\Documents\\GitHub\\leastcostpath\\inst\\extdata\\ASTER_40N.tif")
+b <- terra::rast("C:\\Users\\ppaulse2\\Documents\\GitHub\\leastcostpath\\inst\\extdata\\ASTER_40N_int.tif")
 plot(b, main='DEM check') # requires library(terra), I think, don't forget
  
 slope_cs <- create_slope_cs(x = b, cost_function = "tobler", neighbours = 4) # this is the slow part so far
